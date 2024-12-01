f6330fe1cd49b27e2914f4601c8de543872a6c04
Lirprocs <lirprocs@gmail.com>
2024-12-01 17:10:14 +0300

    update task-6_test.go

--- task-6/task-6_test.go
+++ task-6/task-6_test.go
@@ -1,53 +1,50 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestMerge(t *testing.T) {
-	c1 := make(chan int)
-	c2 := make(chan int)
-	c3 := make(chan int)
-
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	go func() {
-		for _, n := range expected[:3] {
-			c1 <- n
-		}
-		close(c1)
-	}()
-
-	go func() {
-		for _, n := range expected[3:6] {
-			c2 <- n
-		}
-		close(c2)
-	}()
-
-	go func() {
-		for _, n := range expected[6:] {
-			c3 <- n
-		}
-		close(c3)
-	}()
-
-	result := merge(c1, c2, c3)
-
-	var resultSlice []int
-	myMap := make(map[int]bool, len(expected))
-	for v := range result {
-		resultSlice = append(resultSlice, v)
-		myMap[v] = true
+	tests := []struct {
+		expected []int
+	}{
+		{[]int{2, 3, 4, 5, 6, 7, 8, 9}},
+		{[]int{1, 2, 4, 5, 6, 7, 8, 9}},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 9}},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8}},
 	}
+	for _, tt := range tests {
+		c1 := make(chan int)
+		c2 := make(chan int)
+		c3 := make(chan int)
+		go func() {
+			for _, n := range tt.expected[:3] {
+				c1 <- n
+			}
+			close(c1)
+		}()
 
-	if len(resultSlice) != len(expected) {
-		t.Errorf("unexpected length: expected %d, result %d", len(expected), len(resultSlice))
-	}
+		go func() {
+			for _, n := range tt.expected[3:6] {
+				c2 <- n
+			}
+			close(c2)
+		}()
 
-	for _, v := range expected {
-		if !myMap[v] {
-			t.Errorf("missing value: %d", v)
+		go func() {
+			for _, n := range tt.expected[6:] {
+				c3 <- n
+			}
+			close(c3)
+		}()
+		result := merge(c1, c2, c3)
+		resultArr := make([]int, len(tt.expected))
+		for v := range result {
+			resultArr = append(resultArr, v)
+		}
+		if reflect.DeepEqual(resultArr, tt.expected) {
+			t.Errorf("Add() = %d; want %d", resultArr, tt.expected)
 		}
 	}
 
